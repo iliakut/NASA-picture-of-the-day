1917a71dc680479359b1c3f9adcaccb56a877498
ilia <ilyakut@gmail.com>
2020-04-19 23:24:14 +0300

    added header and app

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+import NasaApi from '../../services/NasaAPIService'
+import Header from "../header/header";
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
