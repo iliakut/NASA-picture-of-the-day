16e8a6cf21719fe65f16ea709a0663f76c481efd
ilia <ilyakut@gmail.com>
2020-04-25 14:31:39 +0300

    carousel.js nextClick logic

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, {Component} from "react";
+import CarouselItem from "./carouselItem/carouselItem";
 
-const Carousel = () => {
-  return (
-    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="https://apod.nasa.gov/apod/image/2004/ZGL-200422-0497_1024.jpg" className="d-block" alt="..."/>
+export default class Carousel extends Component {
+  state = {
+    imageItems: [
+      {src: 'https://apod.nasa.gov/apod/image/2004/ZGL-200422-0497_1024.jpg'},
+      {src: 'https://apod.nasa.gov/apod/image/2004/ISS002-E-7377_1024c.jpg'},
+      {src: 'https://apod.nasa.gov/apod/image/2004/EyeOnMW_Claro_960.jpg'},
+    ],
+    activeIndex: 0,
+  };
+
+  onClickNext = () => {
+    const imageItemsLength = this.state.imageItems.length;
+
+    this.setState((state) => {
+      let newActiveIndex = state.activeIndex;
+
+      if (newActiveIndex === imageItemsLength - 1) {
+        newActiveIndex = 0;
+      } else {
+        newActiveIndex += 1;
+      }
+
+      return {
+        activeIndex: newActiveIndex
+      }
+    });
+  };
+
+  render() {
+    const {imageItems, activeIndex} = this.state;
+    const CarouselItems = imageItems.map((item, index) => {
+      return (
+        <CarouselItem
+          key={item.src}
+          src={item.src}
+          isActive={index === activeIndex}
+
+        />
+      );
+    });
+
+    return (
+      <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          {CarouselItems}
         </div>
-        <div className="carousel-item">
-          <img src="https://apod.nasa.gov/apod/image/2004/ISS002-E-7377_1024c.jpg" className="d-block w-100" alt="..."/>
-        </div>
-        <div className="carousel-item">
-          <img src="https://apod.nasa.gov/apod/image/2004/EyeOnMW_Claro_960.jpg" className="d-block w-100" alt="..."/>
-        </div>
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+          onClick={this.onClickNext}
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
       </div>
-      <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-  );
+    )
+  };
 };
-
-export default Carousel;
