96e16d5815e5ef6071d6102466e6c8feac2cb1e1
ilia <ilyakut@gmail.com>
2020-04-26 14:17:27 +0300

    fix style

--- src/components/header/headerDateInput/headerDateInput.js
+++ src/components/header/headerDateInput/headerDateInput.js
@@ -3,7 +3,6 @@ import React, {Fragment} from "react";
 const HeaderDateInput = () => {
   const now = new Date();
   const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()}`;
-  console.log(date)
   return (
     <Fragment>
       <input
@@ -12,10 +11,6 @@ const HeaderDateInput = () => {
         id="pictureDateInput"
         value="2020-03-03"
       />
-      <label className="col-form-label text-primary"
-             htmlFor="pictureDateInput">
-        Enter Date
-      </label>
     </Fragment>
   );
 };
