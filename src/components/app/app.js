fa3c8bdd17913eed9d7cb0e5dd0b87654e23f9a5
ilia <ilyakut@gmail.com>
2020-04-27 19:42:06 +0300

    app.js move to class, add current date

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './app.css';
 import NasaApi from '../../services/NasaAPIService'
 import Header from "../header/header";
 import Carousel from "../carousel/carousel";
 import Footer from "../footer/footer";
 
-function App() {
+class App extends Component {
+  state = {
+    date: this.getNow(),
+  };
 
-  return (
-    <div className="app-text">
-      <Header/>
-      <Carousel/>
-      <Footer/>
-    </div>
-  );
+  getNow() {
+    const now = new Date();
+    const day = (`0${now.getDate()}`).slice(-2);
+    const month = (`0${(now.getMonth() + 1)}`).slice(-2);
+
+    return `${now.getFullYear()}-${month}-${day}`;
+  };
+
+  render() {
+    const {date} = this.state;
+
+    return (
+      <div className="app-text">
+        <Header
+          date={date}
+        />
+        <Carousel/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
