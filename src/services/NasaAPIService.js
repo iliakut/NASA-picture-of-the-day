58717a967bf0deb0cce7833648b5736ade011652
ilia <ilyakut@gmail.com>
2020-04-29 20:32:34 +0300

    NasaAPIService.js fix date param

--- src/services/NasaAPIService.js
+++ src/services/NasaAPIService.js
@@ -1,15 +1,16 @@
 class NasaAPIService {
   _apiBase = 'https://api.nasa.gov/planetary/apod';
 
-  async getData(key, url) {
+  async getData(key, params) {
     // по дефолту вернет сегодняшнюю picture of the day
     const apiKey = key || 'DEMO_KEY';
 
-    const res = await fetch(`${this._apiBase}/?api_key=${apiKey}`);
+    const res = await fetch(`${this._apiBase}/?api_key=${apiKey}${params}`);
     return await res.json();
   }
 
   async getDataInExactDay(date) {
+    console.log(date)
     return this.getData(null, `&date=${date}`)
   }
 
