a70127bd794f51434dfcfae9fb9b279aac840c46
ilia <ilyakut@gmail.com>
2020-05-05 15:05:29 +0300

    added iframe if video

--- src/components/carousel/carouselItem/carouselItem.js
+++ src/components/carousel/carouselItem/carouselItem.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./carouselItem.css"
 
-const CarouselItem = ({src, title}) => {
+const CarouselItem = ({src, title, mediaType}) => {
   return (
     <div className='carousel-item active'>
-      <img src={src}
-           className='d-block rounded mx-auto carousel-item-image'
-           alt='Loading error'
-      />
+      {
+        mediaType === 'image'
+        ? <img
+            src={src}
+            className='d-block rounded mx-auto carousel-item-image'
+            alt='Loading error'
+          />
+        : <iframe
+            title="video"
+            src={src}
+            frameBorder="0"
+
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            className='d-block rounded mx-auto carousel-item-video'
+            allowFullScreen>
+          </iframe>
+      }
       <span className='carouselItem-text'>{title}</span>
     </div>
   );
