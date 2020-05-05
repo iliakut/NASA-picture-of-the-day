54cf59069667e29b1df7ed15c8bcb00960bedf38
ilia <ilyakut@gmail.com>
2020-05-05 15:12:57 +0300

    Updates

---
+++ precache-manifest.b49a578995b754a45fbd94c39482f612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bc1bccb125df7df0dddbc1490f6e61",
+    "url": "/nasa-picture-of-the-day/index.html"
+  },
+  {
+    "revision": "77aeff19dc4514af7dce",
+    "url": "/nasa-picture-of-the-day/static/css/main.af092905.chunk.css"
+  },
+  {
+    "revision": "a17c1e38129d2c0ffd6a",
+    "url": "/nasa-picture-of-the-day/static/js/2.2980f814.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nasa-picture-of-the-day/static/js/2.2980f814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aeff19dc4514af7dce",
+    "url": "/nasa-picture-of-the-day/static/js/main.e7b457c8.chunk.js"
+  },
+  {
+    "revision": "c3dcb081e3d428827237",
+    "url": "/nasa-picture-of-the-day/static/js/runtime-main.8d8548aa.js"
+  }
+]);
